a97e7f1593b34fd6f5754575ee683621c68478f7
333 <iamevanhe@gmail.com>
2024-03-19 17:01:44 +0800

    v0.0.2

--- common/service.sh
+++ common/service.sh
@@ -1,9 +1,13 @@
+# set +x
+SLEEP_TIME=15
+
 while true; do
-  if pgrep com.termux.api > /dev/null; then
+  if pgrep com.termux.api 2>&1 > /dev/null; then
     # echo "com.termux.api is up"
+    sleep $SLEEP_TIME
   else
     # echo "com.termux.api is down"
-    am start -p com.termux.api
+    am start -p com.termux.api > /dev/null
+    sleep $SLEEP_TIME
   fi
-  sleep 15
 done
